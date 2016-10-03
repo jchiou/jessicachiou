d5edb21059f022e3ec08524c20e42b0cc6117526
jchiou <jessica.chiou@gmail.com>
2016-10-02 19:44:48 -0700

    establish app based off of react-boilerplate. start implementing splash page and portfolio content

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,14 +12,126 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import styles from '../App/styles.css';
+import animateCSS from 'animate.css';
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+
+        {/*
+
+            Section: Splash page
+
+        */}
+
+        <section className={styles.home}>
+          <div className={`container ${styles.homecontainer}`}>
+            <div className={styles.wrapper}>
+              <div className="row">
+                <div className="col-sm-offset-2 col-md-8 text-center">
+                  <div className={`wow ${styles.fadeInDown} ${styles.animated}`} data-wow-delay="2s">
+                    <h1 className={styles.contrast}>
+                      <FormattedMessage {...messages.header} />
+                    </h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={`${styles.scrolldown} wow ${styles.fadeInDown} ${styles.animated}`}>
+            <a href="#about">
+              <i className="glyphicon glyphicon-chevron-down"></i>
+            </a>
+          </div>
+
+          <div className={styles.backstretch}>
+            <img
+              role="presentation"
+              className={styles.backgroundpic}
+              src="http://androidwallpape.rs/content/02-wallpapers/154-purplerock/purple-rock-stone-texture-pattern-9-wallpaper.jpg"
+            />
+          </div>
+        </section>
+
+        {/*
+
+            Section: About Me
+
+        */}
+
+        <section className="about-me" id="about">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <div className={`${styles.sectionheading} wow zoomIn ${styles.animated}`} data-wow-delay="0.5">
+                  <h1 className={styles.title}>
+                    <FormattedMessage {...messages.about.header} />
+                  </h1>
+                </div>
+
+
+                <div className="row">
+                  <div className="col-md-6 text-center">
+                    <div className={`${styles.singleabout} wow fadeInUp ${styles.animated} animated`}>
+                      <h3><FormattedMessage {...messages.about.projects.titles[0]} /></h3>
+                      <i><FormattedMessage {...messages.about.projects.oneliner} /></i>
+                      <p>
+                        <FormattedMessage {...messages.about.projects.desc[0]} />
+
+                      </p>
+                      <p className={styles.tags}>
+                        {messages.about.projects.technologies[0].defaultMessage.map((tech) =>
+                          <span>{tech}</span>
+                        )}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-md-6 text-center">
+                    <div className={`${styles.singleabout} wow fadeInUp ${styles.animated} animated`}>
+                      <h3><FormattedMessage {...messages.about.projects.titles[1]} /></h3>
+                      <p>
+                        <FormattedMessage {...messages.about.projects.desc[1]} />
+                      </p>
+                      <p className={styles.tags}>
+                        <span>WebRTC</span> <span>Peer.js</span> <span>React</span> <span>Node.js</span> <span>Bootstrap</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-6 text-center">
+                    <div className={`${styles.singleabout} wow fadeInUp ${styles.animated} animated`}>
+                      <h3><FormattedMessage {...messages.about.projects.titles[2]} /></h3>
+                      <p>
+                        <FormattedMessage {...messages.about.projects.desc[2]} />
+                      </p>
+                      <p className={styles.tags}>
+                        <span>React.js</span> <span>Node.js</span> <span>MongoDB</span> <span>Bootstrap</span>
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-md-6 text-center">
+                    <div className={`${styles.singleabout} wow fadeInUp ${styles.animated} animated`}>
+                      <h3><FormattedMessage {...messages.about.projects.titles[3]} /></h3>
+                      <p>
+                        <FormattedMessage {...messages.about.projects.desc[3]} />
+                      </p>
+                      <p className={styles.tags}>
+                      <span>Node.js</span> <span>React Native</span> <span>PostgreSQL</span> <span>Sequelize</span> <span>Heroku</span>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </div>
     );
   }
 }
--- app/containers/HomePage/messages.js
+++ app/containers/HomePage/messages.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * HomePage Messages
  *
  * This contains all the text for the HomePage component.
@@ -8,6 +8,66 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.components.HomePage.header',
-    defaultMessage: 'This is HomePage components !',
+    defaultMessage: 'Hi, I\'m Jessica.',
   },
+
+  about: {
+    header: {
+      id: 'app.components.HomePage.about.header',
+      defaultMessage: 'Apps I\'ve built:',
+    },
+    projects: {
+      titles:
+        [{
+          id: 'app.components.HomePage.about.projects.CloudCollab',
+          defaultMessage: 'CloudCollab',
+        }, {
+          id: 'app.components.HomePage.about.projects.ReelTime',
+          defaultMessage: 'ReelTime',
+        }, {
+          id: 'app.components.HomePage.about.projects.LaughAboutIt',
+          defaultMessage: 'LaughAboutIt',
+        }, {
+          id: 'app.components.HomePage.about.projects.Unilance',
+          defaultMessage: 'Unilance',
+        }],
+      oneliners: 
+        [{
+          id: 'app.components.HomePage.about.oneliners.CloudCollab',
+          defaultMessage: 'Real-time code collaboration',
+        }, {
+          id: 'app.components.HomePage.about.oneliners.ReelTime',
+          defaultMessage: 'Webcam with friends while watching videos'
+        }]
+      desc:
+        [{
+          id: 'app.components.HomePage.about.projects.desc.CloudCollab',
+          defaultMessage: 'Designed and built React components, Redux containers, actions, and selectors. Enhanced user experience with real time video, messaging. Added extra security layer by sandboxing read-evaluate-print-loop execution.',
+        }, {
+          id: 'app.components.HomePage.about.projects.desc.ReelTime',
+          defaultMessage: 'Increased interactivity by implementing multi-peer P2P calls through a mesh peer network and set up peer server to broker connections securely for deployment',
+        }, {
+          id: 'app.components.HomePage.about.projects.desc.LaughAboutIt',
+          defaultMessage: 'Architected backend REST APIs with integration with PostgreSQL for easy querying into database. Structured database schemas and relationships.',
+        }, {
+          id: 'app.components.HomePage.about.projects.desc.Unilance',
+          defaultMessage: 'Built full stack application using React and Node for rapid prototyping and seamless rendering',
+        }],
+      technologies:
+        [{
+          id: 'app.components.HomePage.about.projects.technologies.CloudCollab',
+          defaultMessage: ['React', 'Redux', 'Github API', 'Socket.io', 'WebRTC'],
+        }, {
+          id: 'app.components.HomePage.about.projects.technologies.ReelTime',
+          defaultMessage: ['WebRTC', 'Peer.js', 'React', 'Node.js', 'Bootstrap'],
+        }, {
+          id: 'app.components.HomePage.about.projects.technologies.LaughAboutIt',
+          defaultMessage: ['Node.js', 'React Native', 'PostgreSQL', 'Sequelize', 'Heroku'],
+        }, {
+          id: 'app.components.HomePage.about.projects.technologies.Unilance',
+          defaultMessage: ['React', 'Node.js', 'MongoDB', 'Bootstrap'],
+        }],
+    },
+  },
+
 });
